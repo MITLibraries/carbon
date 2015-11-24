c50615a3535e4ecaccde67f0b706d1a77efb18c3
Mike Graves <mgraves@mit.edu>
2015-11-24 09:40:25 -0500

    Generate XML feed from person data
    This adds the framework for generating an XML feed of the person
    data. We'll need to figure out the mapping for several of the fields
    in order to complete the record XML.

--- carbon/__init__.py
+++ carbon/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from carbon.app import people
+from carbon.app import people, PersonFeed
 from carbon.db import engine, session
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 
 from click.testing import CliRunner
+from lxml import etree as ET
 import pytest
 
 from carbon.cli import main
@@ -15,8 +16,18 @@ def runner():
     return CliRunner()
 
 
-def test_load_returns_people(runner):
+def test_load_returns_people(runner, E):
+    xml = E.records(
+        E.record(
+            E.field('123456', name='[Proprietary_ID]'),
+            E.field('foobar', name='[Username]')
+        ),
+        E.record(
+            E.field('098754', name='[Proprietary_ID]'),
+            E.field('foobaz', name='[Username]')
+        )
+    )
     res = runner.invoke(main, ['load', '--db',
                         'sqlite:///tests/db/test.db'])
     assert res.exit_code == 0
-    assert 'foobar' in res.output
+    assert res.output.encode('utf-8') == ET.tostring(xml) + b'\n'
