295a93f5eb2b20ce24ae39b473b5b1909efd04eb
Mike Graves <mgraves@mit.edu>
2016-03-28 16:30:12 -0400

    Move database config to top level command
    This will simplify adding commands for multiple feeds.

--- carbon/cli.py
+++ carbon/cli.py
@@ -9,22 +9,26 @@ from carbon.db import engine
 
 @click.group()
 @click.version_option()
-def main():
-    pass
+@click.argument('db')
+def main(db):
+    """Generate feeds for Symplectic Elements.
+
+    The data is pulled from a database identified by DB, which should
+    be a valid SQLAlchemy database connection string. For oracle use:
+
+    oracle://<username>:<password>@<server>:1521/<sid>
+
+    The feed will be printed to stdout.
+
+    See the subcommands for specific feed information.
+    """
+    engine.configure(db)
 
 
 @main.command()
-@click.argument('db')
-@click.option('-o', '--out', help='Output file',
-              default=click.get_binary_stream('stdout'))
-def feed(db, out):
-    """Generate a feed of person data.
-
-    The data is pulled from a database identified by DB, which should
-    be a valid SQLAlchemy database connection string. The feed will be
-    printed to stdout unless a destination file is specified by OUT.
-    """
-    engine.configure(db)
+def hr():
+    """Generate a feed of people data."""
+    out = click.get_binary_stream('stdout')
     with person_feed(out) as f:
         for person in people():
             f(person)
