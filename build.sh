496c829e7a032b751800766db71dd663aa56e7e9
Mike Graves <mgraves@mit.edu>
2018-08-03 15:25:29 -0400

    Add build script for Lambda package
    This adds a build script that can be used to create a Lambda package.
    The cx_Oracle package has two shared library dependencies not present in
    the Lambda execution environment. We can't distribute the Oracle client
    library and it can't be automatically fetched by HTTP. The approach
    taken here is to put both the Oracle client library and libaio into an
    S3 bucket and fetch them at build time.
    
    Building the Lambda package will require the AWS CLI and permissions to
    read the bucket with the shared libs. Given that cx_Oracle and lxml are
    both installed from platform dependent wheels, the build script should
    only be used from a x86_64 linux architecture.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+BUILD_DIR=build-aws
+DIST_DIR=dist-aws
+S3_BUCKET=carbon-deploy
+LIBAIO_SO=libaio.so.1.0.1
+ORACLE_ZIP=instantclient-basiclite-linux.x64-18.3.0.0.0dbru.zip
+
+mkdir -p $BUILD_DIR/lib
+mkdir -p $DIST_DIR
+
+aws s3 cp s3://$S3_BUCKET/$ORACLE_ZIP $BUILD_DIR/$ORACLE_ZIP && \
+  unzip -j $BUILD_DIR/$ORACLE_ZIP -d $BUILD_DIR/lib/ 'instantclient_18_3/*' && \
+  rm $BUILD_DIR/$ORACLE_ZIP
+aws s3 cp s3://$S3_BUCKET/$LIBAIO_SO $BUILD_DIR/lib/$LIBAIO_SO && \
+  ln -rs $BUILD_DIR/lib/$LIBAIO_SO $BUILD_DIR/lib/libaio.so.1 && \
+  ln -rs $BUILD_DIR/lib/libaio.so.1 $BUILD_DIR/lib/libaio.so
+cp -r carbon $BUILD_DIR
+cp lambda.py $BUILD_DIR
+pipenv lock -r > $BUILD_DIR/requirements.txt
+pipenv run pip install -r $BUILD_DIR/requirements.txt -t $BUILD_DIR
+cd $BUILD_DIR && zip --symlinks -r ../$DIST_DIR/carbon.zip *
